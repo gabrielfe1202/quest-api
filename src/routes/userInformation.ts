14e80c81adb7f4b3c39eeec12b50bd9f885eb6e7
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2024-09-17 20:48:39 -0300

    ajuste

--- src/routes/userInformation.ts
+++ src/routes/userInformation.ts
@@ -1,5 +1,4 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
-import { listQuestions } from '../services/listQuestions'
 import z from 'zod'
 import { db } from '../db'
 import { user, userResponses } from '../db/schema'
@@ -10,7 +9,10 @@ const schema = z.object({
 })
 
 class userPoint {
-  constructor(public usuario_id:string,public total_pontos:string){}
+  constructor(
+    public usuario_id: string,
+    public total_pontos: unknown | string
+  ) {}
 }
 
 export const userInformationRoute: FastifyPluginAsyncZod = async app => {
@@ -21,28 +23,50 @@ export const userInformationRoute: FastifyPluginAsyncZod = async app => {
     const resultUsers = await db.select().from(user)
 
     const resultados = await db
-    .select({
+      .select({
         usuario_id: userResponses.userId,
-        total_pontos: sql`SUM(points)`
-    })
-    .from(userResponses)
-    .groupBy(userResponses.userId)
+        total_pontos: sql`SUM(points)`,
+      })
+      .from(userResponses)
+      .groupBy(userResponses.userId)
 
     const listPoints: userPoint[] = []
 
-    resultados.map((item) => listPoints.push(new userPoint(item.usuario_id, item.total_pontos)))
+    resultados.map(item =>
+      listPoints.push(new userPoint(item.usuario_id, item.total_pontos))
+    )
 
-    listPoints.sort((a,b) => parseInt(b.total_pontos) - parseInt(a.total_pontos))    
+    listPoints.sort(
+      (a, b) =>
+        (typeof b.total_pontos === 'string'
+          ? Number.parseInt(b.total_pontos)
+          : 0) -
+        (typeof a.total_pontos === 'string'
+          ? Number.parseInt(a.total_pontos)
+          : 0)
+    )
 
-    let ranking = listPoints.findIndex(x => x.usuario_id == userId) + 1
+    let ranking = listPoints.findIndex(x => x.usuario_id === userId) + 1
 
-    if(ranking == 0){
+    if (ranking === 0) {
       ranking = resultUsers.length
     }
 
+    const score =
+      listPoints.filter(x => x.usuario_id === userId).length > 0
+        ? listPoints.filter(x => x.usuario_id === userId)[0].total_pontos
+        : '0'
+
+    const userName = resultUsers.filter(x => x.id === userId)[0].name
+    const userEmail = resultUsers.filter(x => x.id === userId)[0].email
+
     return {
-      score: parseInt(listPoints.filter(x => x.usuario_id == userId)[0].total_pontos),
-      ranking
+      score: typeof score === 'string' ? Number.parseInt(score) : 0,
+      ranking,
+      userInfos: {
+        userName,
+        userEmail,
+      },
     }
   })
 }
